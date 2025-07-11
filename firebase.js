64b5612547fdf3a6da623ca98ca2d92bbb4ba6ca
fety2116 <yana.fetisova74@gmail.com>
2025-07-10 22:45:41 -0600

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -4,12 +4,12 @@ import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebas
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAQcwegDSfjfuoeKq7s42Eexxj_gGfyQro",
+  apiKey: "AIzaSyAIz4Lcnioo1DAh_S11O1jKC5nyo_ggX5w",
   authDomain: "tennis-schedule-ltc.firebaseapp.com",
   projectId: "tennis-schedule-ltc",
   storageBucket: "tennis-schedule-ltc.firebasestorage.app",
   messagingSenderId: "913456855993",
-  appId: "1:913456855993:web:89af8a5df62d4fda291468"
+  appId: "1:913456855993:web:906283fedb4373bf291468"
 };
 
 const app = initializeApp(firebaseConfig);
